78b6aee450a97b54ce4caac143b7e2190825062f
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-09 13:19:19 +0900

    feat: create Application with rendering Player

---
+++ src/main/java/bowling/NextStepBowlingApplication.java
@@ -0,0 +1,12 @@
+package bowling;
+
+import bowling.domain.Player;
+import bowling.view.InputView;
+
+public class NextStepBowlingApplication {
+
+	public static void main(String[] args) {
+		InputView inputView = new InputView();
+		final Player player = Player.of(inputView.inputPlayerName());
+	}
+}
