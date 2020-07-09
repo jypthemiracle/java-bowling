3ee2be81be4363799b3fe18e6851b5d76a6997c8
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-09 12:58:56 +0900

    feat: InputView to receive PlayerName

---
+++ src/main/java/bowling/domain/PlayerName.java
@@ -0,0 +1,9 @@
+package bowling.domain;
+
+public class PlayerName {
+	private final String name;
+
+	public PlayerName(final String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/bowling/view/InputView.java
@@ -0,0 +1,15 @@
+package bowling.view;
+
+import java.util.Scanner;
+
+import bowling.domain.PlayerName;
+
+public class InputView {
+	private static final String ASK_NAME_MESSAGE = "플레이어 이름은?(3 english letters)";
+	private static final Scanner Scanner = new Scanner(System.in);
+
+	public PlayerName inputPlayerName() {
+		System.out.print(ASK_NAME_MESSAGE);
+		return new PlayerName(Scanner.nextLine());
+	}
+}
